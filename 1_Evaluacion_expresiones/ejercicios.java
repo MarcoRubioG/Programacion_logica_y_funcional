2a0329c622f78ff31aa342d2560c257992f372d4
Marco Esteban Rubio Gómez <themarco27@gmail.com>
2025-09-03 14:26:42 -0600

    avance

---
+++ 1_Evaluacion_expresiones/ejercicios.java
@@ -0,0 +1,11 @@
+//Expresion 1
+//Va a mostar 10 ya que primero musetra antes de incrementar 
+//Si volvemos a impirimir x pero ahora solo ahora si mostrata el incremento en 1
+class Expresion1 {
+    public static void main(String[] args) {
+        int x=10;
+        int b=10;
+        System.out.println(x++);
+        System.out.println(x);
+    }
+}
