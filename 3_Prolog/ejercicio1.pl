085e30348cc127177ef488ad664878c4eea9df4c
Marco Esteban Rubio Gómez <themarco27@gmail.com>
2025-11-06 13:48:52 -0600

    Update ejercicio1.pl

--- 3_Prolog/ejercicio1.pl
+++ 3_Prolog/ejercicio1.pl
@@ -1,2 +1,10 @@
 % CEO y Chairman
 cargo(mark_zuckerberg, ceo_chairman).
+
+% Reportan directamente a Mark Zuckerberg
+reporta_a(sheryl_sandberg, mark_zuckerberg).
+reporta_a(david_wehner, mark_zuckerberg).
+reporta_a(timothy_campos, mark_zuckerberg).
+reporta_a(mike_schroepfer, mark_zuckerberg).
+reporta_a(elliot_schrage, mark_zuckerberg).
+reporta_a(lori_goler, mark_zuckerberg).
