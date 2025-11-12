2135800bafd4a20de370d32e8129ecd7c95777f7
Marco Esteban Rubio Gómez <themarco27@gmail.com>
2025-11-12 14:08:43 -0600

    Reglas

--- 3_Prolog/ejercicio2.pl
+++ 3_Prolog/ejercicio2.pl
@@ -27,3 +27,55 @@ pertenece(chimpanzee, pan, species).
 pertenece(gorilla_gorilla, gorilla, species).
 pertenece(orangutan, pongo, species).
 pertenece(gibbon, hylobates, species).
+
+% REGLAS
+
+% Sacamos los decendientes directos 
+descendiente_directo(X, Y) :- 
+    pertenece(X, Y, _).
+
+% Descendiente indirectos 
+descendiente(X, Y) :- 
+    pertenece(X, Y, _).
+descendiente(X, Y) :- 
+    pertenece(X, Z, _), 
+    descendiente(Z, Y).
+
+% Ancestros
+ancestro(X, Y) :- 
+    descendiente(Y, X).
+
+% Hermanos con el mismo padre
+hermano(X, Y) :- 
+    pertenece(X, P, _), 
+    pertenece(Y, P, _), 
+    X \= Y.
+
+% Misma categoría
+misma_categoria(X, Y, Categoria) :- 
+    pertenece(X, _, Categoria), 
+    pertenece(Y, _, Categoria), 
+    X \= Y.
+
+% Mismo padre 
+mismo_padre(X, Y, Padre) :- 
+    pertenece(X, Padre, _), 
+    pertenece(Y, Padre, _), 
+    X \= Y.
+
+% Todo lo que pertence ese nivel
+de_nivel(X, Nivel) :- 
+    pertenece(X, _, Nivel).
+
+% Ruta desde un elemento hasta la raíz
+ruta(X, [X]) :- 
+    pertenece(X, root, _).
+ruta(X, [X|Resto]) :- 
+    pertenece(X, Padre, _), 
+    Padre \= root, 
+    ruta(Padre, Resto).
+
+% Ancestro común más cercano
+ancestro_comun(X, Y, Ancestro) :- 
+    descendiente(X, Ancestro), 
+    descendiente(Y, Ancestro).
